4d300e83ba47633e32c03aa7c2e0fb2268d13676
Adam Weissman <adam@comedytragedyepic.com>
2019-09-22 00:52:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,10 @@
 # Make your shoe class here!
 
 class Shoe
-  attr_accessor :author, :page_count, :genre
-  attr_reader :brand :color
+  attr_reader :brand, :color
  
   def initialize(brand)
     @brand = brand
-  end
- 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
   end
  
 end
